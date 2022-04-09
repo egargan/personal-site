efde14b5adcee33528ec935c24b462c43b180550
egargan <edwgargan@gmail.com>
2022-04-09 15:34:40 +0100

    feat: make ProjectPreview comp'; add labs to page individually

---
+++ src/routes/labs/_ProjectPreview.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import type { Project } from '$lib/labs';
+  export let project: Project;
+</script>
+
+<article>
+  <video autoplay loop muted class="mb-4 rounded-sm">
+    <source src="{project.image}" type="video/webm">
+    <!-- TODO: add fallback markup! -->
+  </video>
+  <h2 class="font-heading text-xl mb-2">
+    <a class="text-black transition-colors duration-[50ms] hover:text-red"
+      href="{`blog/${project.slug}`}">
+      {project.title}
+    </a>
+  </h2>
+  <p class="mb-3 text-sm">{project.description}</p>
+</article>
--- src/routes/labs/index.svelte
+++ src/routes/labs/index.svelte
@@ -1,41 +1,34 @@
 <script lang="ts">
+  import ProjectPreview from './_ProjectPreview.svelte';
+
   import type { Project } from '$lib/labs';
 
   import flipboard from './flipboard/project';
   import handshake from './handshake/project';
   import recordStack from './record-stack/project';
-
-  const projects: Project[] = [
-    recordStack,
-    flipboard,
-    handshake,
-  ];
 </script>
 
 <ul class="project-grid">
-  {#each projects as project}
-    <div>
-      <article>
-        <video autoplay loop muted>
-          <source src="{project.image}" type="video/webm">
-          <!-- TODO: add fallback markup! -->
-        </video>
-        <h2 class="font-heading text-xl pb-3">
-          <a class="text-black transition-colors duration-[50ms] hover:text-red"
-            href="{`blog/${project.slug}`}">
-            {project.title}
-          </a>
-        </h2>
-        <p class="mb-3 text-sm">{project.description}</p>
-      </article>
-    </div>
-  {/each}
+  <!-- TODO: make this responsive! we'll probably need a component -->
+  <div class="col-start-1 flex flex-col">
+    <li>
+      <ProjectPreview project={recordStack}/>
+    </li>
+  </div>
+  <div class="col-start-2 flex flex-col gap-y-3">
+    <li>
+      <ProjectPreview project={flipboard}/>
+    </li>
+    <li>
+      <ProjectPreview project={handshake}/>
+    </li>
+  </div>
 </ul>
 
 <style>
   .project-grid {
     display: grid;
-    grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
-    grid-gap: 1em;
+    grid-template-columns: 55fr 45fr;
+    grid-gap: 3em;
   }
 </style>
