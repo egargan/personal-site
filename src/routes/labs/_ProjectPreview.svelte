46bc69823c5eb55e1ece0d9a5427b694fa7934fd
egargan <edwgargan@gmail.com>
2022-04-09 16:12:08 +0100

    feat: wrap lab preview content in <a/>, add hover effect

--- src/routes/labs/_ProjectPreview.svelte
+++ src/routes/labs/_ProjectPreview.svelte
@@ -1,18 +1,36 @@
 <script lang="ts">
   import type { Project } from '$lib/labs';
+  import { fade } from 'svelte/transition';
+
   export let project: Project;
+
+  let isHovering = false;
 </script>
 
 <article>
-  <video autoplay loop muted class="mb-4 rounded-sm">
-    <source src="{project.image}" type="video/webm">
-    <!-- TODO: add fallback markup! -->
-  </video>
-  <h2 class="font-heading text-xl mb-2">
-    <a class="text-black transition-colors duration-[50ms] hover:text-red"
-      href="{`blog/${project.slug}`}">
+  <a
+    on:mouseenter={() => { isHovering = true }}
+    on:mouseleave={() => { isHovering = false }}
+    href="{`blog/${project.slug}`}"
+  >
+    <div class="relative mb-4">
+      <video autoplay loop muted class="rounded-sm">
+        <source src="{project.image}" type="video/webm">
+        <!-- TODO: add fallback markup! -->
+      </video>
+      {#if isHovering}
+        <div
+          transition:fade="{{ duration: 40 }}"
+          class="absolute inset-0 rounded-sm opacity-[15%] bg-red"/>
+      {/if}
+    </div>
+    <h2
+      class="font-heading text-xl mb-2 transition-colors duration-[40ms]"
+      class:text-red={isHovering}
+      class:text-black={!isHovering}
+    >
       {project.title}
-    </a>
-  </h2>
-  <p class="mb-3 text-sm">{project.description}</p>
+    </h2>
+    <p class="mb-3 text-sm">{project.description}</p>
+  </a>
 </article>
