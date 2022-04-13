f7d1126771b54b4567f52eb04a2944aceec96b0f
egargan <edwgargan@gmail.com>
2022-04-13 01:05:12 +0100

    feat: add animated scribble for current page indicator

--- src/routes/_nav/NavElement.svelte
+++ src/routes/_nav/NavElement.svelte
@@ -1,12 +1,18 @@
 <script lang="ts">
+  import NavScribble from './NavScribble.svelte';
+
   export let text: string;
   export let path: string;
   export let isCurrentPage: boolean;
+
+  // TODO: slight fade in + out for colour
 </script>
 
 <a class="no-underline {isCurrentPage ? 'text-red' : 'text-black'}" href={path}>
   {text}
 </a>
 {#if isCurrentPage}
-  <div class="w-0.5 h-0.5 bg-red rounded-full mx-auto"></div>
+  <div class="flex justify-center">
+    <NavScribble scale={1.8}/>
+  </div>
 {/if}
