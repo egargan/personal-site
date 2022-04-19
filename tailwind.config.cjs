e80bfde087697d4c47bf50c5861034964e470727
egargan <edwgargan@gmail.com>
2022-04-19 10:59:17 +0100

    feat: tweak font sizes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -22,9 +22,9 @@ module.exports = {
     },
     fontSize: {
       'sm': ['14px', { lineHeight: '21px', letterSpacing: '-0.02em' }],
-      'base': ['16px', { lineHeight: '22px', letterSpacing: '-0.02em' }],
-      'lg': ['18px', { lineHeight: '27px', letterSpacing: '-0.014em' }],
-      'xl': ['22px', { lineHeight: '28px', letterSpacing: '-0.008em' }],
+      'base': ['16px', { lineHeight: '23px', letterSpacing: '-0.012em' }],
+      'lg': ['18px', { lineHeight: '27px', letterSpacing: '-0.01em' }],
+      'xl': ['22px', { lineHeight: '32px', letterSpacing: '-0.008em' }],
       '2xl': ['26px', { lineHeight: '36px', letterSpacing: '-0.006em' }],
       '3xl': ['32px', { lineHeight: '42px', letterSpacing: '-0.004em' }],
       '4xl': ['42px', { lineHeight: '48px', letterSpacing: '-0.004em' }],
