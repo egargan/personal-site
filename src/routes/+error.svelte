d9a48bcc4306c31b96de83a909931f2280ae0d46
egargan <edwgargan@gmail.com>
2023-01-17 23:42:03 +0000

    feat: fill out error page with message and home link

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,6 +1,24 @@
-<script>
-  import { page } from '$app/stores';
+<script lang="ts">
+  import { page } from "$app/stores";
+  import ArrowIcon from "$lib/ArrowIcon.svelte";
+
+  let errorMessage: string;
+
+  if ($page.status === 404) {
+    errorMessage = "This page couldn't be found.";
+  } else {
+    errorMessage = "Something went wrong...";
+  }
 </script>
 
-<h1 class="font-heading text-4xl mb-4">{$page.status}</h1>
-<h2 class="text-2xl mb-4">{$page.status}</h2>
+<div class="h-full flex flex-col justify-center pb-48">
+  <h1 class="font-heading text-4xl mb-4">{$page.status} 🙁</h1>
+  <h2 class="text-2xl mb-4">{errorMessage}</h2>
+  <a
+    href="/"
+    class="text-red font-medium text-lg stroke-red flex gap-x-1 mt-4 items-center"
+  >
+    <ArrowIcon />
+    Go Home
+  </a>
+</div>
