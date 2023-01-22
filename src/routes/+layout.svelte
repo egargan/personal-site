d13643d379df74e4490c477b00b81ac3c7024c7f
egargan <edwgargan@gmail.com>
2023-01-22 16:23:16 +0000

    feat: fix horizontal overflow in blog pages

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,8 +7,7 @@
 </script>
 
 <div
-  class="flex flex-col items-stretch container:items-center font-noto-sans text-base h-screen
-  w-screen"
+  class="flex flex-col items-stretch container:items-center font-noto-sans text-base h-screen"
 >
   <main class="container:w-[840px] h-full w-full px-4">
     <slot />
