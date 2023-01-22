2b5653eeaaf29569da0cafa17b35bc47981db19a
egargan <edwgargan@gmail.com>
2023-01-22 22:36:01 +0000

    tweak: bump root layout side padding for mobile

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,7 +9,7 @@
 <div
   class="flex flex-col items-stretch container:items-center font-noto-sans text-base h-screen"
 >
-  <main class="container:w-[840px] h-full w-full px-4">
+  <main class="container:w-[840px] h-full w-full px-4 sm:px-8">
     <slot />
   </main>
 </div>
