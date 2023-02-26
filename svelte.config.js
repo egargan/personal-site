265cc32df4f35257703d4726d8e29d17de2c5a9d
egargan <edwgargan@gmail.com>
2023-02-26 21:31:35 +0000

    feat: add CSP

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,18 @@
-import adapter from '@sveltejs/adapter-static';
-import preprocess from 'svelte-preprocess';
+import adapter from "@sveltejs/adapter-static";
+import preprocess from "svelte-preprocess";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: preprocess(),
   kit: {
     adapter: adapter(),
-  }
+    csp: {
+      directives: {
+        "default-src": ["self"],
+        "connect-src": ["self", "vitals.vercel-insights.com"],
+      },
+    },
+  },
 };
 
 export default config;
