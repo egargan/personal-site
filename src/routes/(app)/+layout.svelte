dbd34ab60b366d12c587d1c2b775a3c06b49d3e3
egargan <edwgargan@gmail.com>
2023-05-20 21:26:07 +0100

    feat: move pages into (app) dir with nav layout, exclude error page

---
+++ src/routes/(app)/+layout.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    import Nav from "$lib/components/nav/Nav.svelte";
+</script>
+
+<main class="container:w-[840px] h-full w-full px-5 sm:px-8">
+    <Nav />
+    <slot />
+</main>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,7 +5,6 @@
   import { page } from "$app/stores";
 
   import "../app.css";
-    import Nav from "$lib/components/nav/Nav.svelte";
 
   let analyticsId = import.meta.env.VERCEL_ANALYTICS_ID;
 
@@ -25,8 +24,5 @@
 <div
   class="flex flex-col items-stretch container:items-center font-noto-sans text-base h-screen"
 >
-  <main class="container:w-[840px] h-full w-full px-5 sm:px-8">
-    <Nav/>
-    <slot />
-  </main>
+  <slot />
 </div>
