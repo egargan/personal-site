fa3efacbaece78bca47c96d88b01867b3b2fd93e
egargan <edwgargan@gmail.com>
2023-05-21 18:45:39 +0100

    refactor: use gap-x vs mr- in navbar

--- src/lib/components/nav/Nav.svelte
+++ src/lib/components/nav/Nav.svelte
@@ -1,29 +1,29 @@
 <script lang="ts">
-  import { page } from '$app/stores';
-  import NavElement from './NavElement.svelte';
+  import { page } from "$app/stores";
+  import NavElement from "./NavElement.svelte";
 </script>
 
-<nav class="pt-10 pb-6">
-  <ul class="flex justify-end">
-    <li class="mr-10">
+<nav class="pt-10 pb-6 pr-2 sm:pr-4">
+  <ul class="flex justify-end gap-x-10">
+    <li>
       <NavElement
         text="Home"
         path="/"
-        isCurrentPage={$page.url.pathname === '/'}
+        isCurrentPage={$page.url.pathname === "/"}
       />
     </li>
-    <li class="mr-10">
+    <li>
       <NavElement
         text="Blog"
         path="/blog"
-        isCurrentPage={$page.url.pathname.startsWith('/blog')}
+        isCurrentPage={$page.url.pathname.startsWith("/blog")}
       />
     </li>
-    <li class="mr-10">
+    <li>
       <NavElement
         text="Labs"
         path="/labs"
-        isCurrentPage={$page.url.pathname.startsWith('/labs')}
+        isCurrentPage={$page.url.pathname.startsWith("/labs")}
       />
     </li>
   </ul>
