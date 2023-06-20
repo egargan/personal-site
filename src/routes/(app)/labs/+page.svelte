4ecd2cad8da721a1be85671649350d8b7c42791d
egargan <edwgargan@gmail.com>
2023-06-21 00:18:34 +0100

    tweak: lab index layout

--- src/routes/(app)/labs/+page.svelte
+++ src/routes/(app)/labs/+page.svelte
@@ -11,14 +11,14 @@
 </script>
 
 <ul
-  class="grid grid-cols-1 sm:grid-cols-[11fr_9fr] gap-x-10 gap-y-6 md:gap-y-4 px-4 pr-10 sm:px-0
+  class="grid grid-cols-1 sm:grid-cols-[11fr_9fr] gap-x-10 gap-y-6 md:gap-y-4 px-4 pr-10 sm:px-6
   mt-8 md:mt-12"
 >
   <div class="md:col-start-1 flex flex-col gap-y-3 sm:gap-y-3 items-end">
     <li class="sm:translate-y-[-8px]" in:fly={{ y: 15 }}>
       <ProjectPreview project={recordStack} />
     </li>
-    <li class="sm:translate-x-[8px] w-11/12" in:fly={{ y: 15, delay: 60 }}>
+    <li class="sm:translate-x-[8px] sm:w-11/12" in:fly={{ y: 15, delay: 60 }}>
       <ProjectPreview project={handshake} />
     </li>
   </div>
