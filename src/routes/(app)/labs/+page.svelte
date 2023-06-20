32851f85c9e5c79496c81ef2bad706d9fd0f9d8b
egargan <edwgargan@gmail.com>
2023-06-20 23:25:11 +0100

    feat: update lab demo videos, rearrange lab index

--- src/routes/(app)/labs/+page.svelte
+++ src/routes/(app)/labs/+page.svelte
@@ -11,19 +11,23 @@
 </script>
 
 <ul
-  class="grid grid-cols-1 sm:grid-cols-[11fr_9fr] gap-x-10 gap-y-6 md:gap-y-4 px-4 pr-10 sm:px-0 mt-12 md:mt-16"
+  class="grid grid-cols-1 sm:grid-cols-[11fr_9fr] gap-x-10 gap-y-6 md:gap-y-4 px-4 pr-10 sm:px-0
+  mt-8 md:mt-12"
 >
-  <div class="md:col-start-1 flex flex-col gap-y-12 sm:gap-y-3">
+  <div class="md:col-start-1 flex flex-col gap-y-3 sm:gap-y-3 items-end">
     <li class="sm:translate-y-[-8px]" in:fly={{ y: 15 }}>
       <ProjectPreview project={recordStack} />
     </li>
+    <li class="sm:translate-x-[8px] w-11/12" in:fly={{ y: 15, delay: 60 }}>
+      <ProjectPreview project={handshake} />
+    </li>
   </div>
   <div class="md:col-start-2 flex flex-col gap-y-12 sm:gap-y-3">
-    <li class="sm:translate-x-[8px]" in:fly={{ y: 15, delay: 30 }}>
+    <li
+      class="sm:translate-x-[12px] sm:translate-y-4"
+      in:fly={{ y: 15, delay: 30 }}
+    >
       <ProjectPreview project={flipboard} />
-    </li>
-    <li class="sm:translate-x-[-4px]" in:fly={{ y: 15, delay: 60 }}>
-      <ProjectPreview project={handshake} />
     </li>
   </div>
 </ul>
