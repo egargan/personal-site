e75b36922953a5b7bbe77c1972a146e198fff60c
egargan <edwgargan@gmail.com>
2025-12-06 16:51:52 +0000

    feat: add fontaine font fallback plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import mdx from "@astrojs/mdx";
 import { getShikiConfig } from "./src/lib/shiki";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import rehypeSlug from "rehype-slug";
+import { FontaineTransform } from "fontaine";
 
 export default defineConfig({
   integrations: [
@@ -28,5 +29,15 @@ export default defineConfig({
     "/blog/adding-redux-to-svelte": "/posts/adding-redux-to-svelte",
     "/blog/better-errors-ts": "/posts/better-errors-ts",
     "/blog/fzf-yank": "/posts/fzf-yank",
+  },
+  vite: {
+    plugins: [
+      FontaineTransform.vite({
+        fallbacks: {
+          "Atkinson Hyperlegible": ["Arial", "Helvetica Neue", "sans-serif"],
+          "Atkinson Hyperlegible Mono": ["Courier New", "monospace"],
+        },
+      }),
+    ],
   },
 });
